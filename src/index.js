0d8e040e6288be6ba826fdc34de68055caf4c4ef
Timotej Fartek <fartek@outlook.com>
2017-02-01 14:34:47 +0100

    Added babel to transform my ES2015+ to ES5, and fixed some spelling mistakes

---
+++ src/index.js
@@ -0,0 +1,63 @@
+const leftPad = (text) => Array(text + 1).join(' ');
+
+/**
+ * Stringifies objects, arrays and everything in between.
+ * 
+ * @param   {Any}       param           The variable which is to be stringified.
+ * @param   {Integer}   padSize         Number of spaces to left-pad with
+ * @param   {Integer}   initialPadding  The amount of initial spaces for the left-padding.
+ * @returns {String}                    The stringified version of the object, array, whatever was passed in.
+ */
+const PrintableObject = (param, padSize = 2, initialPadding = 0, quoteChar = '\'') =>
+  _PrintableObject(param, padSize, initialPadding, quoteChar, true);
+
+const _PrintableObject = (param, padSize = 2, initialPadding = 0, quoteChar = '\'', initial = true) => {
+  // If the param is an Object (object or array),
+  //   1. Prepend everything with the proper brackets (only once - only if it is initial)
+  //   2. pad everything properly
+  const tabs = initial && typeof param === 'object' ? initialPadding + padSize : initialPadding;
+  if (param && typeof param === 'object' && !Array.isArray(param)) { // If the param is an Object (as in {} and not an array)
+    let objString = initial ? `${leftPad(initialPadding)}{\n` : ''; // Prepend everything with the proper brackets
+    for (const key in param) {
+      if (Object.prototype.hasOwnProperty.call(param, key)) {
+        // For every object's property, check the type,
+        // add the proper amount of padding and the value or
+        // recursively repeat
+        const val = param[key];
+        if (val && typeof val === 'object' && !Array.isArray(val)) { // It is an object (as in {}) and not an array
+          objString += `${leftPad(tabs)}${key}: {\n${_PrintableObject(val, padSize, tabs + padSize, quoteChar, false)}${leftPad(tabs)}},\n`;
+        } else if (val && Array.isArray(val)) { // It is an array
+          objString += `${leftPad(tabs)}${key}: [\n${_PrintableObject(val, padSize, tabs + padSize, quoteChar, false)}${leftPad(tabs)}],\n`;
+        } else if (typeof val === 'number') { // It is a number
+          objString += `${leftPad(tabs)}${key}: ${val},\n`;
+        } else { // Everything else (usually string)
+          objString += `${leftPad(tabs)}${key}: ${quoteChar}${val}${quoteChar},\n`;
+        }
+      }
+    }
+    return `${objString}${initial ? leftPad(initialPadding) + '}\n' : ''}`;
+  } else if (param && Array.isArray(param)) {  // If the param is an array
+    // Do mostly the same stuff as with Objects ({}), but with the square brackets
+    let elems = initial ? `${leftPad(initialPadding)}[\n` : '';
+    for (const elem of param) {
+      if (elem && typeof elem === 'object' && !Array.isArray(elem)) {
+        elems += `${leftPad(tabs)}{\n${_PrintableObject(elem, padSize, tabs + padSize, quoteChar, false)}${leftPad(tabs)}},\n`;
+      } else if (elem && Array.isArray(elem)) {
+        elems += `${leftPad(tabs)}[\n${_PrintableObject(elem, padSize, tabs + padSize, quoteChar, false)}${leftPad(tabs)}],\n`;
+      } else if (typeof elem === 'number') {
+        elems += `${leftPad(tabs)}${elem},\n`;
+      } else {
+        elems += `${leftPad(tabs)}${quoteChar}${elem}${quoteChar},\n`;
+      }
+    }
+    return `${elems}${initial ? leftPad(initialPadding) + ']\n' : ''}`;
+  } else if (param && typeof param === 'number') {
+    // If the param is a number, prepend it with spaces
+    // and don't surround it with quotes
+    return `${leftPad(tabs)}${param}\n`;
+  }
+  // For everything else - surround it with quotes
+  return `${leftPad(tabs)}${quoteChar}${param}${quoteChar}\n`;
+}
+
+module.exports = PrintableObject;
